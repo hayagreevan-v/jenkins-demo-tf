ae163e8ed2776a4fe7057b266af92261e895f007
hayagreevan-v <hayagree@gmail.com>
2025-12-03 11:41:24 +0530

    s3-bucket-2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_s3_bucket" "hex-s3-tf-bucket" {
-  bucket = "hex-s3-tf-bucket"
+  bucket = "hex-s3-tf-bucket-2"
   region = "us-east-1"
   tags = {
     Name = "hex-s3-tf-bucket"
